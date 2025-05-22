a132690a2e394c30cc41b406eaec6e6b384a9ab0
saky-semicolon <saky.aiu22@gmail.com>
2025-05-22 13:41:25 +0800

    changes

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello from Render!"
-
-@app.route("/")
 def index():
     try:
         logger.info("Starting index route")
